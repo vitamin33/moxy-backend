405fdf04a493684cb4a4fc0cade38a4f16b08389
Vitalii Serbyn <vivitseser@gmail.com>
2023-04-15 23:19:54 +0300

    Add string validaitons

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from 'class-validator';
+
 export class CreateRoleDto {
+  @IsString({ message: 'Should be string' })
   readonly name: string;
+  @IsString({ message: 'Should be string' })
   readonly description: string;
 }
--- src/users/dto/change-role.dto.ts
+++ src/users/dto/change-role.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from 'class-validator';
+
 export class ChangeRoleDto {
+  @IsString({ message: 'Should be string' })
   readonly name: string;
+  @IsString({ message: 'Should be string' })
   readonly userId: string;
 }
