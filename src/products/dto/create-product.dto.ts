9d866ab40d0a97c964c8dd4cd3f06aafd96926e1
Vitalii Serbyn <vivitseser@gmail.com>
2023-05-11 13:26:54 +0300

    Add validation for edit and create product dtos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,18 +1,38 @@
-import { IsEnum, ValidateNested } from 'class-validator';
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  Min,
+  ValidateNested,
+} from 'class-validator';
 import { Color } from '../product.entity';
 import { Type } from 'class-transformer';
 
 export class DimensionDto {
   @IsEnum(Color)
+  @IsNotEmpty()
   color: string;
+  @IsInt()
+  @Min(0)
+  @IsNotEmpty()
   quantity: number;
 }
 
 export class CreateProductDto {
+  @IsNotEmpty()
   name: string;
+  @IsNotEmpty()
   idName: string;
+  @IsNotEmpty()
   description: string;
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   costPrice: number;
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   salePrice: number;
   @Type(() => DimensionDto)
   @ValidateNested()
--- src/products/dto/edit-product.dto.ts
+++ src/products/dto/edit-product.dto.ts
@@ -1,12 +1,21 @@
-import { ValidateNested } from 'class-validator';
+import { IsNotEmpty, IsNumber, Min, ValidateNested } from 'class-validator';
 import { DimensionDto } from './create-product.dto';
 import { Type } from 'class-transformer';
 
 export class EditProductDto {
+  @IsNotEmpty()
   name: string;
+  @IsNotEmpty()
   idName: string;
+  @IsNotEmpty()
   description: string;
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   costPrice: number;
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   salePrice: number;
   @Type(() => DimensionDto)
   @ValidateNested()
