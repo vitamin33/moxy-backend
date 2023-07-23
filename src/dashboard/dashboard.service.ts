ab8b8330a4eacf547c6560d3fe92db050aea7bab
Vitalii Serbyn <vivitseser@gmail.com>
2023-07-23 16:42:26 +0300

    Implementation for transforming to week and month coordinates

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -2,10 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { DashboardDto } from './dto/dashboard.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Order, OrderDocument } from 'src/orders/order.entity';
-import { UsersService } from 'src/users/users.service';
-import { ProductsService } from 'src/products/products.service';
 import { Model } from 'mongoose';
-import { getISOWeek } from 'date-fns';
+
+interface RangeData {
+  fromDate: string;
+  toDate: string;
+  key: string;
+  value: number;
+}
+
+interface DateRange {
+  unitNumber: number;
+  fromDate: string;
+  toDate: string;
+}
 
 @Injectable()
 export class DashboardService {
@@ -35,15 +45,24 @@ export class DashboardService {
     const ordersCountByTimeFrame = await this.calculateOrdersCountByTimeFrame(
       fromDate,
       toDate,
+      timeFrame,
     );
 
     // Get the total orders sale value by time frame for graph
     const totalSaleValueByTimeFrame =
-      await this.calculateTotalSaleValueByTimeFrame(fromDate, toDate);
+      await this.calculateTotalSaleValueByTimeFrame(
+        fromDate,
+        toDate,
+        timeFrame,
+      );
 
     // Get the total orders cost value by time frame for graph
     const totalCostValueByTimeFrame =
-      await this.calculateTotalCostValueByTimeFrame(fromDate, toDate);
+      await this.calculateTotalCostValueByTimeFrame(
+        fromDate,
+        toDate,
+        timeFrame,
+      );
 
     return {
       totalSaleValue,
@@ -229,7 +248,8 @@ export class DashboardService {
   private async calculateOrdersCountByTimeFrame(
     fromDate: Date,
     toDate: Date,
-  ): Promise<{ date: Date; value: number }[]> {
+    timef: 'day' | 'week' | 'month',
+  ): Promise<RangeData[]> {
     const dateField = '$createdAt';
 
     const result = await this.orderModel.aggregate([
@@ -251,18 +271,25 @@ export class DashboardService {
         $sort: { _id: 1 },
       },
     ]);
-    // Format the result into an array of { date, value } coordinates
+
     const coordinates = result.map((item) => ({
-      date: item._id,
+      fromDate: item._id,
+      toDate: item._id,
+      key: item._id,
       value: item.count,
     }));
+    const resultCoordinates = transformToTimeFrameCoordinates(
+      coordinates,
+      timef,
+    );
 
-    return coordinates;
+    return resultCoordinates;
   }
   private async calculateTotalSaleValueByTimeFrame(
     fromDate: Date,
     toDate: Date,
-  ): Promise<{ date: Date; value: number }[]> {
+    timef: 'day' | 'week' | 'month',
+  ): Promise<RangeData[]> {
     const dateField = '$createdAt';
 
     const result = await this.orderModel.aggregate([
@@ -316,18 +343,20 @@ export class DashboardService {
       },
     ]);
 
-    // Format the result into an array of { x, y } coordinates
     const coordinates = result.map((item) => ({
-      date: item._id,
+      fromDate: item._id,
+      toDate: item._id,
+      key: item._id,
       value: item.totalSaleValue,
     }));
 
-    return coordinates;
+    return transformToTimeFrameCoordinates(coordinates, timef);
   }
   private async calculateTotalCostValueByTimeFrame(
     fromDate: Date,
     toDate: Date,
-  ): Promise<{ date: Date; value: any }[]> {
+    timef: 'day' | 'week' | 'month',
+  ): Promise<RangeData[]> {
     const dateField = '$createdAt';
 
     const result = await this.orderModel.aggregate([
@@ -381,12 +410,159 @@ export class DashboardService {
       },
     ]);
 
-    // Format the result into an array of { date, totalCostValue } coordinates
+    // Format the result into an array of RangeData coordinates
     const coordinates = result.map((item) => ({
-      date: item._id,
+      fromDate: item._id,
+      toDate: item._id,
+      key: item._id,
       value: item.totalCostValue,
     }));
 
-    return coordinates;
+    return transformToTimeFrameCoordinates(coordinates, timef);
   }
+}
+function transformToTimeFrameCoordinates(
+  coordinates: RangeData[],
+  timef: string,
+) {
+  switch (timef) {
+    case 'week':
+      return compressToWeeks(coordinates);
+    case 'month':
+      return compressToMonths(coordinates);
+    default:
+      return coordinates;
+  }
+}
+
+function compressToWeeks(coordinates: RangeData[]): RangeData[] {
+  const compressedCoordinates: RangeData[] = [];
+  let currentWeekNumber: number | null = null;
+  let currentWeekOrders: number = 0;
+
+  for (const coordinate of coordinates) {
+    const weekRange = getWeekNumberWithRange(coordinate.fromDate);
+
+    if (currentWeekNumber === null) {
+      currentWeekNumber = weekRange.unitNumber;
+      currentWeekOrders = coordinate.value;
+    } else if (currentWeekNumber === weekRange.unitNumber) {
+      currentWeekOrders += coordinate.value;
+    } else {
+      compressedCoordinates.push({
+        fromDate: weekRange.fromDate,
+        toDate: weekRange.toDate,
+        key: `${currentWeekNumber}`,
+        value: currentWeekOrders,
+      });
+
+      currentWeekNumber = weekRange.unitNumber;
+      currentWeekOrders = coordinate.value;
+    }
+  }
+  // Add the last week's data
+  if (currentWeekNumber !== null) {
+    const weekRange = getWeekNumberWithRange(
+      coordinates[coordinates.length - 1].fromDate,
+    );
+    compressedCoordinates.push({
+      fromDate: weekRange.fromDate,
+      toDate: weekRange.toDate,
+      key: `${currentWeekNumber}`,
+      value: currentWeekOrders,
+    });
+  }
+  return compressedCoordinates;
+}
+
+function compressToMonths(coordinates: RangeData[]): RangeData[] {
+  const compressedCoordinates: RangeData[] = [];
+  let currentMonth: string | null = null;
+  let currentMonthOrders: number = 0;
+
+  for (const coordinate of coordinates) {
+    const month = getMonthWithRange(coordinate.fromDate);
+
+    if (currentMonth === null) {
+      currentMonth = month.unitNumber.toString();
+      currentMonthOrders = coordinate.value;
+    } else if (currentMonth === month.unitNumber.toString()) {
+      currentMonthOrders += coordinate.value;
+    } else {
+      compressedCoordinates.push({
+        fromDate: coordinate.fromDate,
+        toDate: coordinate.toDate,
+        key: currentMonth,
+        value: currentMonthOrders,
+      });
+
+      currentMonth = month.unitNumber.toString();
+      currentMonthOrders = coordinate.value;
+    }
+  }
+  // Add the last month's data
+  if (currentMonth !== null) {
+    const month = getMonthWithRange(
+      coordinates[coordinates.length - 1].fromDate,
+    );
+    compressedCoordinates.push({
+      fromDate: month.fromDate,
+      toDate: month.toDate,
+      key: currentMonth,
+      value: currentMonthOrders,
+    });
+  }
+  return compressedCoordinates;
+}
+
+// Function to convert the date in format 'Y-m-d' to week number
+function getWeekNumberWithRange(date: string): DateRange {
+  const targetDate = new Date(date);
+  const firstDayOfYear = new Date(targetDate.getFullYear(), 0, 1);
+  const dayOfWeek = firstDayOfYear.getDay();
+  const weekNumber =
+    Math.floor(
+      (targetDate.getTime() - firstDayOfYear.getTime() - dayOfWeek * 86400000) /
+        604800000,
+    ) + 1;
+
+  // Calculate the start date (fromDate) of the week
+  const fromDate = new Date(
+    targetDate.getFullYear(),
+    0,
+    (weekNumber - 1) * 7 + 1,
+  )
+    .toISOString()
+    .split('T')[0];
+
+  // Calculate the end date (toDate) of the week
+  const toDate = new Date(targetDate.getFullYear(), 0, (weekNumber - 1) * 7 + 7)
+    .toISOString()
+    .split('T')[0];
+
+  return {
+    unitNumber: weekNumber,
+    fromDate,
+    toDate,
+  };
+}
+
+function getMonthWithRange(date: string): DateRange {
+  const [year, month, _] = date.split('-');
+  const monthNumber = parseInt(month);
+
+  // Calculate the start date (fromDate) of the month
+  const fromDate = new Date(`${year}-${month}-01`).toISOString().split('T')[0];
+
+  // Calculate the end date (toDate) of the month
+  const lastDay = new Date(parseInt(year), monthNumber, 0).getDate();
+  const toDate = new Date(`${year}-${month}-${lastDay}`)
+    .toISOString()
+    .split('T')[0];
+
+  return {
+    unitNumber: monthNumber,
+    fromDate,
+    toDate,
+  };
 }
