f3dfc2b5fbdcb14d6cd5625bae4cc78a294d8cc2
Vitalii Serbyn <vivitseser@gmail.com>
2023-08-20 10:21:24 +0300

    fix: fix roles guard exceptions

--- src/modules/auth/roles.guard.ts
+++ src/modules/auth/roles.guard.ts
@@ -4,16 +4,19 @@ import {
   HttpException,
   HttpStatus,
   Injectable,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
 import { ROLES_KEY } from './role-auth.decorator';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -25,17 +28,31 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const req = context.switchToHttp().getRequest();
+
     try {
       const authHeader = req.headers.authorization;
-      const bearer = authHeader.split(' ')[0];
-      const token = authHeader.split(' ')[1];
-      if (bearer !== 'Bearer' || !token) {
-        throw new UnauthorizedException({ message: 'User unauthorized' });
+      if (!authHeader) {
+        throw new HttpException('User unauthorized', HttpStatus.UNAUTHORIZED);
       }
+
+      const [bearer, token] = authHeader.split(' ');
+
+      if (bearer !== 'Bearer' || !token) {
+        throw new HttpException('User unauthorized', HttpStatus.UNAUTHORIZED);
+      }
+
       const user = this.jwtService.verify(token);
       req.user = user;
-      return requiredRoles.includes(user.role.name);
+
+      if (!requiredRoles.includes(user.role.name)) {
+        throw new HttpException('You have not access', HttpStatus.FORBIDDEN);
+      }
+
+      return true;
     } catch (error) {
+      if (error instanceof jwt.TokenExpiredError) {
+        throw new HttpException('User unauthorized', HttpStatus.UNAUTHORIZED);
+      }
       throw new HttpException('You have not access', HttpStatus.FORBIDDEN);
     }
   }
