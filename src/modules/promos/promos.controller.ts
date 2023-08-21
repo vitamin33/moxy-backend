7811388420dbf3bbeae7c3045ef2657983ce7c73
Vitalii Serbyn <vivitseser@gmail.com>
2023-08-21 21:38:08 +0300

    feat: implement promos - get, add, remove

--- src/modules/promos/promos.controller.ts
+++ src/modules/promos/promos.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/role-auth.decorator';
+import { RolesGuard } from '../auth/roles.guard';
+import { AddPromoDto } from './dto/add-promo.dto';
+import { Promo } from './promo.entity';
+import { PromosService } from './promos.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
+@UseGuards(JwtAuthGuard)
 @Controller('promos')
-export class PromosController {}
+export class PromosController {
+  constructor(private readonly promosService: PromosService) {}
+
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
+  @Post('add')
+  @UseInterceptors(FileInterceptor('image'))
+  async addPromo(
+    @Body() addPromoDto: AddPromoDto,
+    @UploadedFile() image,
+  ): Promise<Promo> {
+    return this.promosService.addPromo(addPromoDto, image);
+  }
+
+  @Get()
+  async getPromos(): Promise<Promo[]> {
+    return this.promosService.getPromos();
+  }
+
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
+  @Delete(':id')
+  async removePromo(@Param('id') id: string): Promise<void> {
+    await this.promosService.removePromo(id);
+  }
+}
--- src/modules/promos/promos.service.ts
+++ src/modules/promos/promos.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AddPromoDto } from './dto/add-promo.dto';
+import { Promo } from './promo.entity';
+import { StorageService } from '../storage/storage.service';
 
 @Injectable()
-export class PromosService {}
+export class PromosService {
+  constructor(
+    @InjectModel(Promo.name) private promoModel: Model<Promo>,
+    private storageService: StorageService,
+  ) {}
+
+  async addPromo(dto: AddPromoDto, image: any) {
+    const promo = new this.promoModel(dto);
+
+    if (image) {
+      const imageUrl = await this.storageService.uploadFile(image);
+      promo.imageUrl = imageUrl; // Change images to imageUrl
+    }
+
+    return await promo.save();
+  }
+
+  async getPromos(): Promise<Promo[]> {
+    return this.promoModel.find().exec();
+  }
+
+  async removePromo(id: string): Promise<void> {
+    await this.promoModel.findByIdAndRemove(id).exec();
+  }
+}
